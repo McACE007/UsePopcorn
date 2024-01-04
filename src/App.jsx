27a9d753ad552c2d5763057c355968b5901293fd
mcace007 <mrinalace@gmail.com>
2024-01-04 16:02:35 +0530

    Update: Added error handling for Movie not found and no network or server error

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import ListBox from './components/ListBox';
+import StarRating from './components/StarRating';
 
 const MoviesListBox = ListBox;
 const WatchedMoviesListBox = ListBox;
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+const baseAPIUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=364ec3c5';
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  let query = 'sladfkjlkasdfj';
 
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchMovies() {
+      try {
+        const response = await fetch(`${baseAPIUrl}&s=${query}`);
+
+        if (!response.ok) throw new Error('Something went wrong while fetching the movies');
+
+        const data = await response.json();
+
+        if (data.Response == "False") throw new Error('Movie not found');
+
+        setMovies(data.Search);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchMovies();
+
+  }, []);
 
   return (
     <>
@@ -70,7 +51,9 @@ export default function App() {
 
       <Main>
         <MoviesListBox>
-          <MoviesList movies={movies} />
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MoviesList movies={movies} />}          }
+          {error && <ErrorMessage message={error} />}
         </MoviesListBox>
         <WatchedMoviesListBox>
           <WatchedMovieSummary watched={watched} />
@@ -80,6 +63,21 @@ export default function App() {
     </>
   );
 }
+
+function Loader() {
+  return (
+    <p className='loader'>Loading...</p>
+  );
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className='error'>
+      <span>⛔</span> {message}
+    </p>
+  );
+}
+
 function NavBar({ children }) {
   return (
     <nav className="nav-bar">
